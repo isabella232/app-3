7744c2a775fbc7d9700c0ed8814377ff3bddec31
Chris Meisl <cmeisl@gmail.com>
2019-04-11 09:43:01 -0600

    reuse existing assist instance

--- src/components/Header/Assist.js
+++ src/components/Header/Assist.js
@@ -11,31 +11,35 @@ const updateGlobalState = assistInstance => {
 }
 
 const Assist = async ({ action, expectedNetworkId }) => {
-  let web3
-  try {
-    web3 = await getWeb3()
-  } catch (e) {
-    console.log('failed to get web3')
-  }
-  // dappid is mandatory so will have throw away id for local usage.
-  let testid = 'c212885d-e81d-416f-ac37-06d9ad2cf5af'
-  let assistInstance = await assist.init({
-    dappId: BLOCKNATIVE_DAPPID || testid,
-    web3: web3,
-    networkId: expectedNetworkId,
-    images: {
-      welcome: {
-        src: 'https://kickback.events/card.png',
-        srcset: 'Welcome to Kickback'
-      },
-      complete: {
-        src: 'https://kickback.events/card.png',
-        srcset: 'Now you are ready to use Kickback'
-      }
+  const provider = await getProvider()
+  let assistInstance = provider.state.assist
+  if (!assistInstance) {
+    let web3
+    try {
+      web3 = await getWeb3()
+    } catch (e) {
+      console.log('failed to get web3')
     }
-  })
+    // dappid is mandatory so will have throw away id for local usage.
+    let testid = 'c212885d-e81d-416f-ac37-06d9ad2cf5af'
+    assistInstance = await assist.init({
+      dappId: BLOCKNATIVE_DAPPID || testid,
+      web3: web3,
+      networkId: expectedNetworkId,
+      images: {
+        welcome: {
+          src: 'https://kickback.events/card.png',
+          srcset: 'Welcome to Kickback'
+        },
+        complete: {
+          src: 'https://kickback.events/card.png',
+          srcset: 'Now you are ready to use Kickback'
+        }
+      }
+    })
 
-  updateGlobalState(assistInstance)
+    updateGlobalState(assistInstance)
+  }
 
   let state
   let result = {
@@ -57,27 +61,15 @@ const Assist = async ({ action, expectedNetworkId }) => {
     let {
       correctNetwork,
       currentProvider,
-      legacyWallet,
-      legacyWeb3,
       mobileDevice,
-      modernWallet,
-      modernWeb3,
-      supportedNetwork,
       accountAddress,
-      web3Version,
       web3Wallet
     } = state
     result = {
       correctNetwork,
       currentProvider,
-      legacyWallet,
-      legacyWeb3,
       mobileDevice,
-      modernWallet,
-      modernWeb3,
-      supportedNetwork,
       accountAddress,
-      web3Version,
       web3Wallet,
       ...result
     }
